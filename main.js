0fc20908831fb97f6cfac72d1b5c7906dbd7c570
harkeetb <93454413+harkeetb@users.noreply.github.com>
2023-04-30 15:01:56 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+/*
+    main.js
+    Purpose: Implement the logic for the R-P-S game, both for human player and AI computer player.
+    Author: Harkeet Bal
+*/
+
+// Global variables:
+
+let computerChoice;
+let playerChoice;
+
+let playerScore = 0;
+let computerScore = 0;
+
+
+
