ae2dc1bf0a0e1777b4bee1c0457bb39d44b9a32a
harkeetb <93454413+harkeetb@users.noreply.github.com>
2023-05-01 14:57:05 -0500

    Implemented computer players random choice

--- main.js
+++ main.js
@@ -21,14 +21,15 @@ const computer = document.querySelector("#computer-score");
 const output = document.querySelector("#output");
 player.textContent = "Player score : ${playerScore}";
 computer.textContent = "Computer score: ${computerScore}";
-output.textContent = "Have fun";
+output.textContent = "First to 3 wins. Have fun!";
 
 
 // button logic:
 buttons.forEach((button)=>{button.addEventListener
     ('click', ()=>{
         playerChoice = button.id;
-
+        
+        // check which action was chosen
         switch(playerChoice){
             case "rock":
                 playerChoiceInt = 0;
@@ -46,5 +47,20 @@ buttons.forEach((button)=>{button.addEventListener
     })
 })
 
-function computerTurn(computerChoice);
-function play();
+function computerTurn(computerChoice){
+    let rand = Math.floor(Math.random() * 3); // random number from 0-2
+    computerChoice = {value: 0}; // initialize a field that contains the computers choice (0,1,2 for r,p,s)
+
+    // check which action was taken and update the field
+    if(rand == 0){
+        computerChoice.value = "rock";
+    }
+    else if(rand == 1){
+        computerChoice.value = "paper";
+    }
+    else{
+        computerChoice.value = "scissors";
+    }
+
+    return rand;
+}
